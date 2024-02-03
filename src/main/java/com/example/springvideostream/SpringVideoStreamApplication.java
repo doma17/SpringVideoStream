19b419d643a3b7a7e2a3f9e4f5a521e5e3d5d188
doma17 <rovin1273@gmail.com>
2024-02-03 15:23:25 +0900

    Initial commit

---
+++ src/main/java/com/example/springvideostream/SpringVideoStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvideostream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVideoStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springvideostream/SpringVideoStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvideostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVideoStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
